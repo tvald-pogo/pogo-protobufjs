9cd4ed1cb91479628db7c26764952a73afbb1688
Tony Valderrama <tvald@neon-arcade.co>
2016-07-25 18:38:50 -0700

    Fix internal path to proto schemas.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let walk = require('walk').walkSync;
 let Protobuf = require('protobufjs');
 let fs = require('fs');
 
-let builder = Protobuf.newBuilder();
-builder.importRoot = './src';
+let src = './POGOProtos/src/POGOProtos';
 
-walk('./src/POGOProtos', { listeners: {
+let builder = Protobuf.newBuilder();
+builder.importRoot = src;
+
+walk(src, { listeners: {
   file: (root, stats, next) => {
     let path = `${root}/${stats.name}`;
     if (stats.name.endsWith('.proto'))
