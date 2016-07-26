b31954e00762ad1cfde3c1528c8ec38ea539345e
Tony Valderrama <tvald@neon-arcade.co>
2016-07-25 20:18:45 -0700

    Tidy up debugging output. Export API equivalent to that of pokemongo-protobuf module.

--- index.js
+++ index.js
@@ -7,21 +7,56 @@ let src = './POGOProtos/src';
 let builder = Protobuf.newBuilder();
 builder.importRoot = src;
 
+// add all types to the builder
 walk(src, { listeners: {
   file: (root, stats, next) => {
-    let path = `${root}/${stats.name}`;
     if (stats.name.endsWith('.proto'))
-    {
-      console.log(`load ${path}`);
-      Protobuf.loadProtoFile(path, builder);
-      next();
-    }
-    else {
-      console.log(`skip ${path}`);
-      next();
-    }
+      Protobuf.loadProtoFile(`${root}/${stats.name}`, builder);
+    next();
   },
 }});
 
+// build types
+let types = builder.build();
 
-exports = builder.build();
+// walk type structure to build up list of type names
+let typeMap = {};
+let typeNames = [];
+
+let walk_types = (name, obj) => {
+  switch(typeof(obj))
+  {
+    case 'function':
+      if (obj.hasOwnProperty('encode') && obj.hasOwnProperty('decode'))
+      {
+        console.log(name);
+        typeMap[name] = obj;
+        typeNames.push(name);
+      }
+    case 'object':
+      Object.keys(obj).forEach(childName => {
+        if (!childName.startsWith('encode') && !childName.startsWith('decode'))
+          walk_types(`${name}.${childName}`, obj[childName]);
+      });
+      break;
+  }
+};
+walk_types('POGOProtos', types.POGOProtos);
+
+// export API
+exports.types = types;
+
+exports.resolve = (type) => {
+  if (typeMap[type]) return typeMap[type];
+  throw new Error(`Failed to resolve type ${type}`);
+};
+
+exports.parse = (buffer, type) => {
+  return exports.resolve(type).decode(buffer);
+};
+
+exports.serialize = (data, type) => {
+  return exports.resolve(type).encode(data);
+};
+
+exports.info = () => typeNames;
